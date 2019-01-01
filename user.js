ce187b9524b6a662c03516139c0104e099a99fb3
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2019-01-01 21:13:05 +0100

    Setting privacy.firstparty.isolate to true prevents cookie deletion in uMatrix
    https://github.com/gorhill/uMatrix/issues/878

--- user.js
+++ user.js
@@ -170,7 +170,7 @@ user_pref("security.sri.enable",				true);
 user_pref("network.http.referer.spoofSource",			true);
 // user_pref("network.http.referer.XOriginPolicy",		2);
 user_pref("network.cookie.cookieBehavior",			1);
-user_pref("privacy.firstparty.isolate",				true);
+//user_pref("privacy.firstparty.isolate",				true);
 user_pref("network.cookie.thirdparty.sessionOnly",		true);
 //user_pref("general.useragent.override",				"Mozilla/5.0 (Windows NT 6.1; rv:45.0) Gecko/20100101 Firefox/45.0");
 //user_pref("general.appname.override",				"Netscape");
