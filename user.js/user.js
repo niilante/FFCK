f02940bb59eb6afc1b24fbcd369f8af3cff9d3ff
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2019-03-27 19:15:44 +0100

    Firefox 66.0.2 fixes Web Compatibility issues with Office 365
    https://bugzilla.mozilla.org/show_bug.cgi?id=1521370
    https://bugzilla.mozilla.org/1538966

--- user.js/user.js
+++ user.js/user.js
@@ -6,7 +6,6 @@
 //user_pref("privacy.resistFingerprinting.reduceTimerPrecision.jitter", true);
 //
 user_pref("dom.webnotifications.requireuserinteraction", true);
-user_pref("dom.keyboardevent.keypress.hack.use_legacy_keycode_and_charcode", "powerpoint.officeapps.live.com");
 //user_pref("layout.css.isolation.enabled", true); //default
 user_pref("intl.charset.fallback.utf8_for_file", false);
 //user_pref("browser.chrome.errorReporter.enabled", false); //default
