14cd62e90df95aefcaa044d4a03bc54867867c68
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2019-04-02 14:04:42 +0200

    Firefox no longer adds temporary exceptions for invalid certificates
    Keep in mind that even if the flag is changed FF will only accept it temporarily, you can't choose the certificate for each site.
    
    https://bugzilla.mozilla.org/show_bug.cgi?id=1492498

--- user.js/user.js
+++ user.js/user.js
@@ -5,6 +5,7 @@
 //user_pref("privacy.resistFingerprinting.jsmloglevel", "Warn");
 //user_pref("privacy.resistFingerprinting.reduceTimerPrecision.jitter", true);
 //
+user_pref("security.certerrors.permanentOverride", false);
 user_pref("dom.webnotifications.requireuserinteraction", true);
 //user_pref("layout.css.isolation.enabled", true); //default
 user_pref("intl.charset.fallback.utf8_for_file", false);
