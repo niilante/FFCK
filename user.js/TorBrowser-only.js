ef5302de77a52df41a45b3320b9594f177608c6b
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2019-11-08 15:56:26 +0000

    Added TorBrowser-only.js

---
+++ user.js/TorBrowser-only.js
@@ -0,0 +1,10 @@
+//
+//  TorBrowser-only.js by CHEF-KOCH     
+//    
+//      Don't use Firefox forks, except the Tor Browser!     
+//      This is for Tor Browser Bundle only, do not use it on a normal Firefox version.     
+//      
+//      Version 0.01 
+//    
+//      TODO: Upload, fix and optimize.
+//
