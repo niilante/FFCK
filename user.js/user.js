2e87f339f8a3c1bfd60f46c42bbcc72f7be76492
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2020-06-01 18:04:09 +0200

    The browser.urlbar.update1 pref to turn off the Megabar got removed in FF 77
    ftp.mozilla.org/firefox/releases/77.0/
    ftp.mozilla.org/firefox/releases/68.9.0esr/
    
    Which means we can remove it, use CSS "tweak" instead. See workarounds.

--- user.js/user.js
+++ user.js/user.js
@@ -48,7 +48,7 @@ user_pref("browser.newtabpage.activity-stream.feeds.sections", false);
 user_pref("widget.wayland-dmabuf-webgl.enabled", true);
 user_pref("widget.wayland-dmabuf-textures.enable", true);
 user_pref("widget.wayland-dmabuf-vaapi.enabled", true);
-user_pref("browser.urlbar.update1", false); // will be removed in FF 77
+//user_pref("browser.urlbar.update1", false);
 user_pref("dom.webgpu.enabled", false);
 user_pref("browser.urlbar.openViewOnFocus", false);
 user_pref("network.netlink.route.check.IPv4", "");
